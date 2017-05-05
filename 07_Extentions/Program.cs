48bf125ed6a209c8ebae3589390797d8b8f91204
Larry Lai <yanlin.lai@afusion.com>
2017-05-05 17:32:07 +0800

    Extension methods

--- 07_Extentions/Program.cs
+++ 07_Extentions/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/// <summary>
+/// Extension Methods (C# Programming Guide)
+/// https://msdn.microsoft.com/en-us//library/bb383977.aspx
+/// </summary>
+/// <remarks>Need to compare this with Apple Swift extention</remarks>
 namespace _07_Extentions
 {
+    using ExtensionMethods;
+
     class Program
     {
         static void Main(string[] args)
         {
+            int[] ints = { 10, 45, 15, 39, 21, 26 };
+            var result = ints.OrderBy(g => g);
+            foreach (var i in result)
+            {
+                System.Console.Write(i + " ");
+            }
+
+            //=========================================
+
+            string s = "Hello Extension Methods";
+            int count = s.WordCount();
+            Console.WriteLine("WordCount() : " + count);
+        }
+        // 10 15 21 26 39 45 WordCount() : 3
+    }
+}
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
         }
     }
 }
