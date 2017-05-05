c4e64df583f7b6b4b9027878dfe2d388ba298fe1
Larry Lai <yanlin.lai@afusion.com>
2017-05-05 15:38:58 +0800

    NuGet LINQPad for Dump()

---
+++ 04_Nullable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Nullable
+{
+    class Program
+    {
+        public class Example
+        {
+            public int? MyMethod()
+            {
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Type t = typeof(Example);
+            MethodInfo mi = t.GetMethod("MyMethod");
+            Type retval = mi.ReturnType;
+            Console.WriteLine("Return value type ... {0}", retval);
+            Type answer = Nullable.GetUnderlyingType(retval);
+            Console.WriteLine("Underlying type ..... {0}", answer);
+
+            /**
+             * 
+             Return value type ... System.Nullable`1[System.Int32]
+             Underlying type ..... System.Int32
+
+             * */
+        }
+        /*
+        This code example produces the following results:
+
+        Return value type ... System.Nullable`1[System.Int32]
+        Underlying type ..... System.Int32
+
+        */
+    }
+}
---
+++ 05_DoubleQuestionMark/Program.cs
@@ -0,0 +1,87 @@
+﻿using LINQPad;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// http://stackoverflow.com/questions/11412639/double-question-marks-vs-if-when-assigning-same-var
+/// </summary>
+namespace _05_DoubleQuestionMark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = new object();
+            var B = new object();
+
+            var iterations = 1000000000;
+
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < iterations; i++)
+            {
+                if (i == 1) sw.Start();
+                if (A == null)
+                {
+                    A = B;
+                }
+            }
+            sw.Stop();
+            TimeSpan first = sw.Elapsed;
+
+            sw.Reset();
+            for (int i = 0; i < iterations; i++)
+            {
+                if (i == 1) sw.Start();
+                A = A ?? B;
+            }
+            sw.Stop();
+            TimeSpan second = sw.Elapsed;
+
+            first.Dump();
+            second.Dump();
+
+            (first.TotalMilliseconds / second.TotalMilliseconds).Dump("Ratio");
+
+            //==============
+
+            new TimeSpan(1, 2, 3, 4, 5).ToFriendlyDisplay(3).Dump();
+            new TimeSpan(0, 5, 3, 4, 5).ToFriendlyDisplay(3).Dump();
+        }
+    }
+
+    /// <summary>
+    /// http://stackoverflow.com/questions/1138723/timespan-to-friendly-string-library-c
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        private enum TimeSpanElement
+        {
+            Millisecond,
+            Second,
+            Minute,
+            Hour,
+            Day
+        }
+
+        public static string ToFriendlyDisplay(this TimeSpan timeSpan, int maxNrOfElements)
+        {
+            maxNrOfElements = Math.Max(Math.Min(maxNrOfElements, 5), 1);
+            var parts = new[]
+                            {
+                            Tuple.Create(TimeSpanElement.Day, timeSpan.Days),
+                            Tuple.Create(TimeSpanElement.Hour, timeSpan.Hours),
+                            Tuple.Create(TimeSpanElement.Minute, timeSpan.Minutes),
+                            Tuple.Create(TimeSpanElement.Second, timeSpan.Seconds),
+                            Tuple.Create(TimeSpanElement.Millisecond, timeSpan.Milliseconds)
+                        }
+                                        .SkipWhile(i => i.Item2 <= 0)
+                                        .Take(maxNrOfElements);
+
+            return string.Join(", ", parts.Select(p => string.Format("{0} {1}{2}", p.Item2, p.Item1, p.Item2 > 1 ? "s" : string.Empty)));
+        }
+    }
+}
