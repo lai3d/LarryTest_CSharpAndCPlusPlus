7e9cc50e9f59e5a9d41c4fee1091bb7af23c9fc4
Larry Lai <yanlin.lai@afusion.com>
2017-05-05 16:24:20 +0800

    NuGet more packages to play with.

---
+++ 08_OracleDataAccess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_OracleDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Orale Data Access later!");
+        }
+    }
+}
