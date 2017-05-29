0126bb4d7970fa416b05c87a5805e5484e361e9a
Larry Lai <yanlin.lai@afusion.com>
2017-05-29 17:24:37 +0800

    Multiple lines string literal

---
+++ 14_String/StringTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_String
+{
+    class StringTest
+    {
+        static void Main(string[] args)
+        {
+            string query = @"SELECT foo, bar
+FROM table
+WHERE id = 42";
+
+            Console.WriteLine(query);
+        }
+    }
+
+//SELECT foo, bar
+//FROM table
+//WHERE id = 42
+
+}
