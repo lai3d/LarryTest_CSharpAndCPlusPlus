8a3c86e55eedf16001f6d2508f6573e6c4b42b98
Larry Lai <yanlin.lai@afusion.com>
2017-07-14 13:35:32 +0800

    Get the method-level attributes

--- 16_Attribute_Custom/CustomAttributeTest.cs
+++ 16_Attribute_Custom/CustomAttributeTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,28 @@ namespace _16_Attribute_Custom
         {
             // Call function to get and display the attribute.
             GetAttribute(typeof(CustomAttributeTest));
+
+            GetAttributeMethodLevel(typeof(CustomAttributeTest));
+
+            //The Name Attribute is: Joan Smith.
+
+            //The Level Attribute is: 42.
+            //The Reviewed Attribute is: True.
+            //The Name Attribute on the class level is: Joan Smith.
+            //The Level Attribute on the class level is: 42.
+            //The Reviewed Attribute on the class level is: True.
+
+            //No attribute in member function Void GetAttribute(System.Type).
+
+            //No attribute in member function Void GetAttribute2(System.Type).
+
+            //No attribute in member function System.String ToString().
+
+            //No attribute in member function Boolean Equals(System.Object).
+
+            //No attribute in member function Int32 GetHashCode().
+
+            //No attribute in member function System.Type GetType().
         }
 
         public static void GetAttribute(Type t)
@@ -32,6 +55,53 @@ namespace _16_Attribute_Custom
                 Console.WriteLine("The Level Attribute is: {0}.", MyAttribute.Level);
                 // Get the Reviewed value.
                 Console.WriteLine("The Reviewed Attribute is: {0}.", MyAttribute.Reviewed);
+            }
+        }
+
+        public static void GetAttributeMethodLevel(Type t)
+        {
+            DeveloperAttribute att;
+
+            // Get the class-level attributes.
+
+            // Put the instance of the attribute on the class level in the att object.
+            att = (DeveloperAttribute)Attribute.GetCustomAttribute(t, typeof(DeveloperAttribute));
+
+            if (att == null)
+            {
+                Console.WriteLine("No attribute in class {0}.\n", t.ToString());
+            }
+            else
+            {
+                Console.WriteLine("The Name Attribute on the class level is: {0}.", att.Name);
+                Console.WriteLine("The Level Attribute on the class level is: {0}.", att.Level);
+                Console.WriteLine("The Reviewed Attribute on the class level is: {0}.\n", att.Reviewed);
+            }
+
+            // Get the method-level attributes.
+
+            // Get all methods in this class, and put them
+            // in an array of System.Reflection.MemberInfo objects.
+            MemberInfo[] MyMemberInfo = t.GetMethods();
+
+            // Loop through all methods in this class that are in the
+            // MyMemberInfo array.
+            for (int i = 0; i < MyMemberInfo.Length; i++)
+            {
+                att = (DeveloperAttribute)Attribute.GetCustomAttribute(MyMemberInfo[i], typeof(DeveloperAttribute));
+                if (att == null)
+                {
+                    Console.WriteLine("No attribute in member function {0}.\n", MyMemberInfo[i].ToString());
+                }
+                else
+                {
+                    Console.WriteLine("The Name Attribute for the {0} member is: {1}.",
+                        MyMemberInfo[i].ToString(), att.Name);
+                    Console.WriteLine("The Level Attribute for the {0} member is: {1}.",
+                        MyMemberInfo[i].ToString(), att.Level);
+                    Console.WriteLine("The Reviewed Attribute for the {0} member is: {1}.\n",
+                        MyMemberInfo[i].ToString(), att.Reviewed);
+                }
             }
         }
     }
@@ -79,3 +149,4 @@ public class DeveloperAttribute : Attribute
         set { reviewed = value; }
     }
 }
+ 
